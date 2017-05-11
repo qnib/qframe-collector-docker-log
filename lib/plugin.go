c76eaeb71a6a68cb6a03eed744a282447fd3882e
Christian Kniep <christian@qnib.org>
2017-05-11 16:29:53 +0200

    refine docker-logs to connect to already running containers

--- lib/plugin.go
+++ lib/plugin.go
@@ -2,13 +2,17 @@ package qframe_collector_docker_log
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"strings"
+	"time"
+
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/events"
 	"github.com/zpatrick/go-config"
+
 	"github.com/qnib/qframe-types"
-	"context"
 )
 
 const (
@@ -27,6 +31,7 @@ var (
 // -> allow for gracefully shutdown of the supervisor
 type ContainerSupervisor struct {
 	Plugin
+	Action      string
 	CntID 		string 			 // ContainerID
 	CntName 	string			 // sanatized name of container
 	Container 	types.ContainerJSON
@@ -36,32 +41,38 @@ type ContainerSupervisor struct {
 }
 
 
-func (p *Plugin) StartSupervisor(CntID, CntName string, cnt types.ContainerJSON) {
+func (p *Plugin) StartSupervisor(ce events.Message, cnt types.ContainerJSON) {
 	s := ContainerSupervisor{
 		Plugin: *p,
-		CntID: CntID,
-		CntName: CntName,
+		Action: ce.Action,
+		CntID: ce.Actor.ID,
+		CntName: ce.Actor.Attributes["name"],
 		Container: cnt,
 		Com: make(chan interface{}),
 		cli: p.cli,
 		qChan: p.QChan,
 	}
-	p.sMap[CntID] = s
+	p.sMap[ce.Actor.ID] = s
 	go s.Run()
 }
 
 func (p *Plugin) StartSupervisorCE(ce qtypes.ContainerEvent) {
-	p.StartSupervisor(ce.Event.Actor.ID, ce.Event.Actor.Attributes["name"], ce.Container)
+	p.StartSupervisor(ce.Event, ce.Container)
 }
 
 func (cs ContainerSupervisor) Run() {
 	msg := fmt.Sprintf("Start listener for: '%s' [%s]", cs.CntName, cs.CntID)
-	cs.Log("notice", msg)
+	cs.Log("info", msg)
 
-	reader, err := cs.cli.ContainerLogs(ctx, cs.CntID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
+	logOpts := types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true, Tail: "all"}
+	if cs.Action == "running" {
+		logOpts.Tail = "0"
+	}
+	reader, err := cs.cli.ContainerLogs(ctx, cs.CntID, logOpts)
 	if err != nil {
 		msg := fmt.Sprintf("Error when connecting to log of %s: %s", cs.CntName, err.Error())
 		cs.Log("error", msg)
+		return
 	}
 	scanner := bufio.NewScanner(reader)
     for scanner.Scan() {
@@ -106,7 +117,45 @@ func New(qChan qtypes.QChan, cfg config.Config, name string) (Plugin, error) {
 	return p, err
 }
 
+func (p *Plugin) SubscribeRunning() {
+	cnts, err := p.cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		p.Log("error", fmt.Sprintf("Failed to list containers: %s", err.Error()))
+	} else {
+		skipLabel := p.CfgStringOr("skip-container-label", "org.qnib.qframe.skip-log")
+		for _, cnt := range cnts {
+			cjson, err := p.cli.ContainerInspect(ctx, cnt.ID)
+			if err != nil {
+				continue
+			}
+			// Skip those with the label:
+			skipCnt := false
+			for label, _ := range cjson.Config.Labels {
+				if label == skipLabel {
+					p.Log("info", fmt.Sprintf("Skip subscribing to logs of '%s' as label '%s' is set", cnt.Names, skipLabel))
+					skipCnt = true
+					break
+				}
+			}
+			if skipCnt {
+				continue
+			}
+			event := events.Message{
+				Type:   "container",
+				Action: "running",
+				Actor: events.Actor{
+					ID: cnt.ID,
+					Attributes: map[string]string{"name": strings.Trim(cnt.Names[0],"/")},
+				},
+			}
+			ce := qtypes.NewContainerEvent(qtypes.NewTimedBase(p.Name, time.Unix(cnt.Created, 0)), cjson, event)
+			p.StartSupervisorCE(ce)
+		}
+	}
+}
 func (p *Plugin) Run() {
+	p.Log("notice", fmt.Sprintf("Start v%s", p.Version))
+
 	var err error
 	dockerHost := p.CfgStringOr("docker-host", "unix:///var/run/docker.sock")
 	p.cli, err = client.NewClient(dockerHost, dockerAPI, nil, nil)
@@ -119,13 +168,13 @@ func (p *Plugin) Run() {
 		p.Log("error", fmt.Sprintf("Error during Info(): %v >err> %s", info, err))
 		return
 	} else {
-		p.Log("notice", fmt.Sprintf("Connected to '%s' / v'%s' (SWARM: %s)", info.Name, info.ServerVersion, info.Swarm.LocalNodeState))
+		p.Log("info", fmt.Sprintf("Connected to '%s' / v'%s' (SWARM: %s)", info.Name, info.ServerVersion, info.Swarm.LocalNodeState))
 	}
 	// need to start listener for all containers
 	skipRunning := p.CfgBoolOr("skip-running", false)
 	if ! skipRunning {
 		p.Log("info", fmt.Sprintf("Start listeners for already running containers: %d", info.ContainersRunning))
-		// TODO: Fire up listeners
+		p.SubscribeRunning()
 	}
 	inputs := p.GetInputs()
 	srcSuccess := p.CfgBoolOr("source-success", true)
